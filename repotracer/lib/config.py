22d78aefddcb8f53908b0519857b2875d6bfdbc7
Amedee d'Aboville <amedee.daboville@gmail.com>
2023-07-09 17:54:21 -0400

    Actually pretend there is a config file being read

--- repotracer/lib/config.py
+++ repotracer/lib/config.py
@@ -1,7 +1,11 @@
-def get_config(repo_name, stat_name):
+def read_config_file():
     # Let's pretend we open a big config file with a bunch of stats in it
     config_data = {
-        "storage_location": "./repos",
+        "repo_storage_location": "./repos",
+        "stat_storage": {
+            "type": "csv",
+            "path": "./stats",  # will store stats in ./stats/<repo_name>/<stat_name>.csv
+        },
         "repos": [
             {
                 "name": "svelte",
@@ -16,6 +20,15 @@ def get_config(repo_name, stat_name):
             }
         ],
     }
+    return config_data
+
+
+def get_repo_storage_location():
+    return read_config_file()["storage_location"] or "./repos"
+
+
+def get_config(repo_name, stat_name):
+    config_data = read_config_file()
     repo_config = next(
         (x for x in config_data["repos"] if x["name"] == repo_name), None
     )
