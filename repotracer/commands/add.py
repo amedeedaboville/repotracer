c7d42d6efc962121a1bd4b88cbe34d109a4e0c9c
Amedee d'Aboville <amedee.daboville@gmail.com>
2023-08-27 18:08:00 -0400

    Checkpoint: a lot more work on the add-repo command, which works kind of nicely now.

--- repotracer/commands/add.py
+++ repotracer/commands/add.py
@@ -2,12 +2,17 @@ import os
 import shutil
 import typer
 
+from collections import namedtuple
 from repotracer.commands import run
 from repotracer.lib import git, config
 from rich import print
 from rich.console import Console
-from typing import Optional
+from typing import Optional, List
 from typing_extensions import Annotated
+import click
+import questionary
+from enum import Enum
+
 from urllib.parse import urlparse
 
 
@@ -23,7 +28,7 @@ def add_repo(url_or_path: str, name: Optional[str] = None):
     if is_url(url_or_path):
         repo_name = name or os.path.splitext(os.path.basename(url_or_path))[0]
         repo_storage_path = os.path.join("./repos", repo_name)
-        it.download_repo(url=url_or_path, repo_path=repo_storage_path)
+        git.download_repo(url=url_or_path, repo_path=repo_storage_path)
     else:
         repo_name = name or os.path.basename(url_or_path)
         repo_storage_path = os.path.join("./repos", repo_name)
@@ -38,31 +43,155 @@ def add_repo(url_or_path: str, name: Optional[str] = None):
     # and call add_stat() if they do
 
 
+qstyle = questionary.Style(
+    [
+        (
+            "highlighted",
+            "reverse bold",
+        )  # pointed-at choice in select and checkbox prompts
+    ]
+)
+
+
 def add_stat(
-    repo_name: Annotated[
-        str, typer.Option(prompt="What repo do you want to add a stat for?")
-    ],
-    stat_name: Annotated[
-        str, typer.Option(prompt="What name do you want to give your stat?")
-    ] = None,
-    stat_type: Optional[str] = None,
+    repo_name: Annotated[Optional[str], typer.Argument()] = None,
+    stat_name: Annotated[Optional[str], typer.Argument()] = None,
 ):
     if repo_name is None:
-        if repo_name not in config.list_repos():
-            should_add = typer.confirm(
-                f"The repo {repo_name} was not found in the config. Would you like to add it now?"
-            )
-            if should_add:
-                add_repo(repo_name)
-            else:
-                return
+        repo_name = questionary.select(
+            "Which repo do you want to add a new stat for?",
+            choices=config.list_repos() + ["<new repo>"],
+            style=qstyle,
+            qmark="🕵️",
+        ).ask()
+        if repo_name == "<new repo>":
+            repo_name = questionary.text(
+                "What name do you want to give the repo?"
+            ).ask()
+            add_repo(repo_name)
 
-    if stat_type is None:
-        stat_type = typer.prompt(
-            "What kind of stat do you want to add? (built-in or custom)"
+    if stat_name is None:
+        stat_name = questionary.text("What name do you want to give the stat?").ask()
+    if stat_name in config.list_stats_for_repo(repo_name):
+        print(
+            f"The stat '{stat_name}' already exists for the repo '{repo_name}'. Please choose a different name."
         )
-    # show them the list of built-in stats, or they can put in a custom one
-    # If there are any parameters for the stat, ask the user for them
-    # store this info in the config
-    # ask the user if they want to run the stat now
-    pass
+        return
+
+    # TODO make this be able to be given from the command line, for now this command
+    # will require user input
+
+    stat_type = questionary.select(
+        "What type of stat do you want to add?",
+        choices=["regex_count", "file_count", "custom_script"],
+        style=qstyle,
+        # qmark="?"
+    ).ask()
+
+    stat_params = promt_build_stat(stat_type)
+    print("Building the following stat:")
+    print(stat_params)
+    stat_config = config.StatConfig(
+        name=stat_name,
+        description=stat_params.pop("description", None),
+        type=stat_type,
+        params=stat_params,
+        path_in_repo=stat_params.pop("path_in_repo", None),
+    )
+    config.add_stat(repo_name, stat_config)
+
+
+ParamOption = namedtuple("ParamOption", ["name", "description", "required"])
+
+
+def promt_build_stat(stat_type: str):
+    common_stat_options = [
+        ParamOption(
+            name="description",
+            description="A description of the stat, used in the title of generated graphs",
+            required=False,
+        ),
+        ParamOption(
+            name="start",
+            description="The start date for the stat, if you don't want to start at the beginning of the repo",
+            required=False,
+        ),
+        ParamOption(
+            name="path_in_repo",
+            description="The path in the repo to run the stat on",
+            required=False,
+        ),
+    ]
+    # Todo move this into the definition of the measurement/stat type
+    params_by_type = {
+        "regex_count": [
+            ParamOption(
+                name="pattern",
+                description="The regex pattern to pass to ripgrep",
+                required=True,
+            ),
+            ParamOption(
+                name="ripgrep_args",
+                description="(Optional) any additional ripgrep args. Useful to exclude files with -g ",
+                required=False,
+            ),
+        ],
+        "file_count": ["file_extension"],
+        "custom_script": ["script"],
+    }
+    stat_options = params_by_type[stat_type]
+    required_stat_params = [option for option in stat_options if option.required]
+    optional_stat_params = [option for option in stat_options if not option.required]
+    stat_params = prompt_required_options(required_stat_params)
+    print(stat_params)
+    stat_params |= prompt_for_options(
+        f"The type {stat_type} has the following options. Would you like to set any of these?",
+        optional_stat_params,
+    )
+    print(stat_params)
+    common_options = prompt_for_options(
+        f"Additionally, stats can have the following options. Would you like to set any of these?",
+        common_stat_options,
+    )
+    print(stat_params, common_options)
+    return {**common_options, "params": stat_params}
+
+
+def prompt_required_options(options: List[ParamOption]):
+    choices = {}
+    for option in options:
+        choices[option.name] = prompt_for_single_param_option(option)
+    return choices
+
+
+def find(list, predicate):
+    for item in list:
+        if predicate(item):
+            return item
+    return None
+
+
+def prompt_for_options(prompt: str, options: List[ParamOption]):
+    options_chosen = {}
+    options_remaining = {option.name for option in options}
+    while options_remaining:
+        option_name = questionary.select(
+            prompt,
+            choices=list(options_remaining) + ["<none>"],
+            style=qstyle,
+            qmark="📈️",
+        ).ask()
+        if option_name == "<none>":
+            break
+        option_value = prompt_for_single_param_option(
+            find(options, lambda x: x.name == option_name)
+        )
+        options_chosen[option_name] = option_value
+        options_remaining.remove(option_name)
+    return options_chosen
+
+
+def prompt_for_single_param_option(option: ParamOption):
+    return questionary.text(
+        f"Please choose a value for '{option.name}': {option.description}."
+    ).ask()
--- repotracer/lib/config.py
+++ repotracer/lib/config.py
@@ -40,7 +40,7 @@ config_file_contents = None
 def read_config_file():
     global config_file_contents
     if config_file_contents:
-        return default_config | config_file_contents
+        return default_config | (config_file_contents or {})
     # print("Using default config.")
     try:
         print("Looking for config file at", os.path.abspath(get_config_path()))
@@ -48,7 +48,7 @@ def read_config_file():
             config_file_contents = json5.load(f)  # python 3.9 operator for dict update
     except FileNotFoundError:
         print(f"Could not find config file at {get_config_path()}. Keeping defaults.")
-    return default_config | config_file_contents
+    return default_config | (config_file_contents or {})
 
 
 def get_repo_storage_location():
@@ -98,7 +98,7 @@ def get_config(repo_name, stat_name) -> (RepoConfig, str):
 
 def write_config_file(config):
     with open(get_config_path(), "w") as f:
-        json5.dump(config, f, indent=4)
+        json5.dump(config, f, indent=4, quote_keys=True)
     config_file_contents = config
 
 
@@ -109,4 +109,25 @@ def add_repo(repo_name: str, repo_path: str):
         "path": repo_path,
         "stats": {},
     }
+    write_config_file(config)
+
+
+def remove_nones(d: dict[Any, Any]):
+    return {k: v for k, v in d.items() if v is not None}
+
+
+def add_stat(repo_name: str, stat_config: StatConfig):
+    config = read_config_file()
+    repo_config = config["repos"][repo_name]
+    if "stats" not in repo_config:
+        repo_config["stats"] = {}
+    repo_config["stats"][stat_config.name] = remove_nones(
+        {
+            "description": stat_config.description,
+            "type": stat_config.type,
+            "params": stat_config.params,
+            "path_in_repo": stat_config.path_in_repo,
+        }
+    )
+
     write_config_file(config)
