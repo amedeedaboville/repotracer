aa7ce440f0f23e7f8c709a78426298f7a702864f
Amedee d'Aboville <amedee.daboville@gmail.com>
2023-10-28 20:12:48 -0400

    Actually git add the new test file

---
+++ tests/test_cli_works.py
@@ -0,0 +1,13 @@
+from typer.testing import CliRunner
+
+from repotracer.main import app
+
+runner = CliRunner()
+
+
+def test_help_text_works():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+    print(result.stdout)
+    assert "Usage: " in result.stdout
+/Users/amedee/.repotracer/stats/postgres/count-c.png
