035920f919bc98d56591d98d5a015c59b1d47352
Amedee d'Aboville <amedee.daboville@gmail.com>
2024-01-05 17:33:22 +0100

    Fold measurement_fns.py into measurment.py. These were meant to be together.

--- repotracer/lib/measurement.py
+++ repotracer/lib/measurement.py
@@ -1,13 +1,99 @@
+from . import git
+from collections import namedtuple
 from dataclasses import dataclass
 from typing import Callable, Generic, TypeVar, Type, Any, TypedDict
-from abc import ABC, abstractmethod
-import pandas as pd
-from tqdm.auto import tqdm
-from datetime import datetime
-import functools
 from typing_extensions import Protocol
-from .measurement_fns import rg_count, fd_count, tokei_count, script_auto
-from collections import namedtuple
+import json
+import subprocess
+
+##############################################################################
+## Actual measurement functions
+##############################################################################
+
+
+def script(cmd):
+    return lambda: subprocess.check_output(cmd, shell=True)
+
+
+def script_now(cmd):
+    try:
+        return subprocess.check_output(cmd, shell=True).decode("utf-8")
+    except subprocess.CalledProcessError as e:
+        # ignoGenericre exit code 1
+        if e.returncode == 1:
+            return e.output
+        print("Error in script_now")
+        print(e)
+        print(e.output)
+        raise e
+
+
+def float_or_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        try:
+            return float(s)
+        except Exception as e:
+            return float("NaN")
+
+
+def script_auto(cmd, return_type):
+    output = script_now(cmd)
+    if return_type == "number":
+        return {"output": float_or_int(output)}
+    elif return_type == "json":
+        return json.loads(output)
+    else:
+        raise ValueError(f"Unknown return type {return_type}")
+
+
+def ripgrep_count_file(pattern):
+    return int(script(f"rg -l {pattern} | wc -l"))
+
+
+def rg_count(pattern: str, rg_args: str) -> int:
+    filenames_with_counts = script_now(f"rg '{pattern}' --count {rg_args or ''}")
+    res = {
+        "total": sum(
+            int(line.split(":")[-1]) for line in filenames_with_counts.splitlines()
+        )
+    }
+    return res
+
+
+def fd_count(pattern: str, extra_cli_args: str) -> int:
+    filenames_with_counts = script_now(
+        f"fd --glob '{pattern}' --type file {extra_cli_args or ''}"
+    )
+    # todo: eventually, store the individual file names as intermediate results
+    res = {"total": len(filenames_with_counts.splitlines())}
+    return res
+
+
+def tokei_count(languages: str, extra_cli_args: str) -> int:
+    if languages == "all":
+        languages = ""
+    language_arg = f"-t {languages}" if languages else ""
+    json_out = script_now(f"tokei --output json {language_arg} {extra_cli_args or ''}")
+    parsed = json.loads(json_out)
+    code_totals = {language: data["code"] for language, data in parsed.items()}
+    breakdown_languages = True
+    if breakdown_languages:
+        del code_totals["Total"]
+        return code_totals
+    else:
+        return code_totals["Total"]
+
+
+def jsx_to_tsx():
+    return script_now("tokei --output json --languages jsx,tsx,js,ts")
+
+
+##############################################################################
+# Measurement Configs
+# and the dictionary of all measurements
+##############################################################################
 
 
 class MeasurementConfig(object):
@@ -74,6 +160,8 @@ ScriptMeasurement = FunctionMeasurement[ScriptConfig](
 ParamOption = namedtuple("ParamOption", ["name", "description", "required"])
 MeasurementDef = namedtuple("MeasurementDef", ["obj", "params"])
 
+# todo ideally we could remove the duplication between the TypedDict subclasses (eg FileCountConfig)
+# and these ParamOption lists, which both store the same information about which parameters a measurment takes.
 all_measurements = {
     "regex_count": MeasurementDef(
         obj=RegexMeasurement,
