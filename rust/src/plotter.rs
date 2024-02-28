fcd28b1cdd7f0a83da14ad5a85833724a5009f90
Amedee d'Aboville <amedee.daboville@gmail.com>
2024-02-28 16:35:15 -0500

    Make the generated charts a lot less ugly

--- rust/src/plotter.rs
+++ rust/src/plotter.rs
@@ -1,10 +1,50 @@
 use std::fs;
 
-use crate::{commands::run, config::get_stats_dir};
-use chrono::DateTime;
+use crate::config::get_stats_dir;
 use plotters::prelude::*;
-use plotters::style::{Palette, Palette99, ShapeStyle};
-use polars::{datatypes::StaticArray, prelude::*};
+use plotters::style::{Color, Palette, RGBColor};
+use polars::prelude::*;
+
+struct SeabornDeepPalette;
+impl Palette for SeabornDeepPalette {
+    const COLORS: &'static [(u8, u8, u8)] = &[
+        (76, 114, 176),  // Blue
+        (85, 168, 104),  // Green
+        (196, 78, 82),   // Red
+        (129, 114, 178), // Purple
+        (204, 185, 116), // Yellow
+        (100, 181, 205), // Cyan
+        //The rest are palette99
+        (230, 25, 75),
+        (60, 180, 75),
+        (255, 225, 25),
+        (0, 130, 200),
+        (245, 130, 48),
+        (145, 30, 180),
+        (70, 240, 240),
+        (240, 50, 230),
+        (210, 245, 60),
+        (250, 190, 190),
+        (0, 128, 128),
+        (230, 190, 255),
+        (170, 110, 40),
+        (255, 250, 200),
+        (128, 0, 0),
+        (170, 255, 195),
+        (128, 128, 0),
+        (255, 215, 180),
+        (0, 0, 128),
+        (128, 128, 128),
+        (0, 0, 0),
+    ];
+
+    fn pick(idx: usize) -> PaletteColor<Self>
+    where
+        Self: Sized,
+    {
+        PaletteColor::<Self>::pick(idx)
+    }
+}
 
 pub fn plot(
     repo_name: &str,
@@ -53,31 +93,40 @@ pub fn plot(
                 "{repo_name}:{stat_name} by repotracer at {}",
                 run_at.to_rfc3339()
             ),
-            ("sans-serif", 20).into_font(),
+            ("sans-serif", (4).percent_height()),
         )
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
+        .margin(5.percent())
+        .margin_bottom(3.percent())
+        .set_label_area_size(LabelAreaPosition::Left, (5).percent())
+        .set_label_area_size(LabelAreaPosition::Bottom, (5).percent())
         .build_cartesian_2d(start_time..end_time, min_value..max_value)?;
 
-    println!("ABOUT TO CHART");
-    chart.configure_mesh().draw()?;
+    chart
+        .configure_mesh()
+        .max_light_lines(0)
+        .label_style(("sans-serif", 3.percent_height()))
+        .x_desc("Date")
+        .axis_desc_style(TextStyle::from(("sans-serif", 25).into_font()).color(&BLACK.mix(0.4)))
+        .y_label_formatter(&|y| format!("{:.0}", y))
+        .x_label_formatter(&|x| {
+            let seconds = *x / 1000;
+            let naive = chrono::NaiveDateTime::from_timestamp(seconds, 0);
+            let datetime: chrono::DateTime<chrono::Utc> =
+                chrono::DateTime::from_utc(naive, chrono::Utc);
+            format!("{}", datetime.format("%Y-%m-%d"))
+        })
+        .draw()?;
 
     let x_data = time_series.into_no_null_iter().collect::<Vec<_>>();
-    let palette_size = Palette99::COLORS.len(); // Get the size of the palette
-    let mut color_index = 0; // Initialize a color index
+    let palette_size = SeabornDeepPalette::COLORS.len(); // Get the size of the palette
 
-    for series in df.get_columns() {
-        let name = series.name();
-        println!("CHARTING {name}, {}", series.dtype());
+    for (idx, series) in df.get_columns().iter().enumerate() {
         if !series.dtype().is_numeric() {
             println!(
-                "Skipping plotting series {name} as its type {} is not numeric.",
+                "Skipping plotting series {} as its type {} is not numeric.",
+                series.name(),
                 series.dtype()
             );
-        }
-        if name == "commit" || name == "commit_time" {
-            continue;
         }
         let values: Vec<f64> = series
             .cast(&DataType::Float64)
@@ -88,25 +137,19 @@ pub fn plot(
             .collect();
 
         let data = x_data.iter().zip(values.iter()).map(|(&x, &y)| (x, y));
+        let color = SeabornDeepPalette::pick(idx % palette_size).mix(0.9);
 
-        println!("generating lineseries");
-
-        let color = Palette99::pick(color_index % palette_size);
-        color_index += 1; // Move to the next color for the next series
-
-        let style = ShapeStyle::from(color).filled();
         chart
-            .draw_series(LineSeries::new(data, style))?
-            .label(format!("{}", name))
-            .legend(move |(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], style.clone()));
-
-        println!("DRAWING");
-        chart
-            .configure_series_labels()
-            .background_style(&WHITE.mix(0.8))
-            .border_style(&BLACK)
-            .draw()?;
+            .draw_series(LineSeries::new(data, color.stroke_width(5)))?
+            .label(series.name())
+            .legend(move |(x, y)| Rectangle::new([(x, y - 5), (x + 10, y + 5)], color.filled()));
     }
+    chart
+        .configure_series_labels()
+        .label_font(("sans-serif", 20)) // Adjust the font size here
+        .background_style(&WHITE)
+        .border_style(&BLACK)
+        .draw()?;
     root.present()?;
     Ok(())
 }
