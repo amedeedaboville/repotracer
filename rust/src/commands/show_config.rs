794fe7f3d0a77a4d469eb65607ddfe7f77a18850
Amedee d'Aboville <amedee.daboville@gmail.com>
2024-10-11 15:04:56 +0200

    Add missing files

---
+++ rust/src/commands/show_config.rs
@@ -0,0 +1,7 @@
+use clap::Args;
+use crate::config::get_config_path;
+
+pub fn show_config_command() {
+    let config_path = get_config_path();
+    println!("Config file is located at: {}", config_path.display());
+}
